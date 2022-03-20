543488d2cca55e956eccf941c7896e963013a298
Dmitrii Zorin <zchakiz@gmail.com>
2022-03-20 17:04:21 +0300

    commit pods

---
+++ Classes/TestPod.swift
@@ -0,0 +1,15 @@
+//
+//  TestPod.swift
+//  TestPodChack
+//
+//  Created by Zorin Dmitrii on 20.03.2022.
+//
+
+import Foundation
+
+public struct One {
+    public init() {}
+    public func oneFunc() {
+        print("Hello World!!!")
+    }
+}
